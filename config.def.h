f3795b41bb91e44e04530fddcb29e9650c64d7ff
Konstantin Kostin <kkostin@azul.com>
2022-08-13 23:13:18 +0700

    [xrdb] Apply dwm-xrdb-6.2.diff

--- config.def.h
+++ config.def.h
@@ -8,15 +8,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -71,7 +72,6 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-modi", "window,ssh,run,drun", "-show", "run", "-sidebar-mode", "-terminal", "st", "-theme", "dracula", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
@@ -112,6 +112,7 @@ static Keychord keychords[] = {
 	{1, {{MODKEY|ShiftMask,             XK_equal,   }}, setgaps,        {.i = 0  } },
 	{1, {{MODKEY,                       XK_r,       }}, spawn,          {.v = rulayoutcmd } },
 	{1, {{MODKEY,                       XK_u,       }}, spawn,          {.v = uslayoutcmd } },
+	{1, {{MODKEY,                       XK_F5,      }}, xrdb,           {.v = NULL } },
 	TAGKEYS1(                           XK_1,                           0)
 	TAGKEYS1(                           XK_2,                           1)
 	TAGKEYS1(                           XK_3,                           2)
